201dba95e1c6506d0f99ad6a772b6819b126f89c
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-12-27 14:04:53 -0500

    Todo Fix

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-audio_file = open("audio_example.wav", "rb")
+audio_file = open("audio.mp3", "rb")
 transcribed = openai.Audio.transcribe("whisper-1", audio_file)
 print("Resultado: ", transcribed.text)
