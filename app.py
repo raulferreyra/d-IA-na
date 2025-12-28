dada56910aab6404fb6577f8a3d5929743a1788d
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-12-27 20:31:34 -0500

    create base windows

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-import os
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+'''import os
 import openai
 
 from dotenv import load_dotenv
 
-'''load_dotenv()
+load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 audio_file = open("audio.mp3", "rb")
 transcribed = openai.Audio.transcribe("whisper-1", audio_file)
 print("Resultado: ", transcribed.text)'''
+
+def main():
+    app = QApplication(sys.argv)
+
+    win = QMainWindow()
+    win.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
