f2b6890e41762e723624fba019f58e97dddadccf
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-12-27 20:30:50 -0500

    commet code, creating the window

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import openai
 
 from dotenv import load_dotenv
 
-load_dotenv()
+'''load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 audio_file = open("audio.mp3", "rb")
 transcribed = openai.Audio.transcribe("whisper-1", audio_file)
-print("Resultado: ", transcribed.text)
+print("Resultado: ", transcribed.text)'''
